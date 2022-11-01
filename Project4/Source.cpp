377ee4165f89fbab5532457814b2cd5bb688e28e
Sofiia213 <nazarenkosofia21@gmail.com>
2022-11-01 18:46:21 +0200

    Добавьте файлы проекта.

---
+++ Project4/Source.cpp
@@ -0,0 +1,32 @@
+// Lab_03_3.cpp
+// < Назаренко Софія >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 24
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double R; // вхідний параметр
+	double y; // результат обчислення виразу
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x <= 0)
+		y = ((-R / 6) * (x + 6));
+	else
+		if (0 < x && x <= R)
+			y = -sqrt(pow(R, 2) - pow(R, 2));
+		else
+			if (R < x && x <= 2*R)
+				y = R + sqrt(pow(R,2)-(x-2*pow(R,2)));
+			else
+					y = R + -R/(2*R)*(x-0);
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
